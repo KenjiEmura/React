62c50a2459b96fe89a4d6176e92a9002a9cc4cb0
Kenji Emura <kenjiemura@gmail.com>
2022-10-06 13:35:04 +0900

    Completed the 'Add User' functionality

--- modals/src/App.js
+++ modals/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
 
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState(DUMMY_USERS);
+
+  const addUserHandler = (createdUser) => {
+    setUsersList((prevUsersList) => {
+      return [createdUser, ...prevUsersList];
+    });
+    console.log(usersList);
+  };
   return (
     <div>
-
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
 
 export default App;
+
+const DUMMY_USERS = [
+  { id: "0", name: "Kenji Emura", age: 31 },
+  { id: "1", name: "Hernando Rojas", age: 21 },
+];
