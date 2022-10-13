b9fc587f34ff1a8bee863747743d5f052720db68
Kenji Emura <kenjiemura@gmail.com>
2022-10-13 17:26:23 +0900

    Added an email input field and dummy validation

--- forms-and-user-input/src/components/SimpleInput.js
+++ forms-and-user-input/src/components/SimpleInput.js
@@ -1,33 +1,73 @@
 import { useState } from "react";
 
 const SimpleInput = (props) => {
+  // Name field
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameWasTouched, setEnteredNameWasTouched] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== "";
-  const shouldShowError = !enteredNameIsValid && enteredNameWasTouched;
+  const shouldShowErrorInNameField =
+    !enteredNameIsValid && enteredNameWasTouched;
+  const nameInputClasses = `form-control ${
+    shouldShowErrorInNameField ? "invalid" : ""
+  }`;
 
   const nameInputChangeHandler = (event) => {
     setEnteredNameWasTouched(true);
     setEnteredName(event.target.value);
   };
 
-  const formSumbissionHandler = (event) => {
-    event.preventDefault();
-    setEnteredNameWasTouched(true);
-    if (!enteredNameIsValid) {
-      return;
-    }
-    console.log("State: ", enteredName);
-    setEnteredName("");
-    setEnteredNameWasTouched(false);
-  };
-
   const nameInputBlurHandler = () => {
     setEnteredNameWasTouched(true);
   };
 
-  const nameInputClasses = `form-control ${shouldShowError ? "invalid" : ""}`;
+  // Email field
+  const [enteredMail, setEnteredMail] = useState("");
+  const [enteredMailWasTouched, setEnteredMailWasTouched] = useState(false);
+
+  const errorToShowInMailField = enteredMailWasTouched
+    ? enteredMail.length < 5
+      ? "Please enter a mail with at least 5 characters"
+      : !enteredMail.includes("@")
+      ? "The email must contain '@'"
+      : null
+    : null;
+
+  console.log(errorToShowInMailField);
+  const shouldShowErrorInMailField =
+    errorToShowInMailField && enteredMailWasTouched;
+  const mailInputClasses = `form-control ${
+    shouldShowErrorInMailField ? "invalid" : ""
+  }`;
+
+  const mailInputChangeHandler = (event) => {
+    setEnteredMailWasTouched(true);
+    setEnteredMail(event.target.value);
+  };
+
+  const mailInputBlurHandler = () => {
+    setEnteredMailWasTouched(true);
+  };
+
+  // Form validation and submission
+  let formIsValid = false;
+  if (enteredNameIsValid && !errorToShowInMailField) {
+    formIsValid = true;
+  }
+
+  const formSumbissionHandler = (event) => {
+    event.preventDefault();
+    setEnteredNameWasTouched(true);
+    if (!formIsValid) {
+      return;
+    }
+    console.log("Name: ", enteredName.trim());
+    console.log("Email: ", enteredMail.trim());
+    setEnteredName("");
+    setEnteredNameWasTouched(false);
+    setEnteredMail("");
+    setEnteredMailWasTouched(false);
+  };
 
   return (
     <form onSubmit={formSumbissionHandler}>
@@ -40,12 +80,25 @@ const SimpleInput = (props) => {
           onBlur={nameInputBlurHandler}
           value={enteredName}
         />
-        {shouldShowError && (
+        {shouldShowErrorInNameField && (
           <p className="error-text">Name must not be empty</p>
         )}
       </div>
+      <div className={mailInputClasses}>
+        <label htmlFor="email">Your email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={mailInputChangeHandler}
+          onBlur={mailInputBlurHandler}
+          value={enteredMail}
+        />
+        {shouldShowErrorInMailField && (
+          <p className="error-text">{errorToShowInMailField}</p>
+        )}
+      </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
