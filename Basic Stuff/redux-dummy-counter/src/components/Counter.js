da78d889de628720a226d9273bb330cbc534644c
Kenji Emura <kenjiemura@gmail.com>
2022-10-14 13:00:58 +0900

    Migrated State Management to Redux toolkit

--- Basic Stuff/redux-dummy-counter/src/components/Counter.js
+++ Basic Stuff/redux-dummy-counter/src/components/Counter.js
@@ -1,8 +1,17 @@
 import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 
+import { counterActions } from "../store";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  // ↓ This state is just to test that React is included with the redux toolkit package
+  const [isShown, setIsShown] = useState(true);
+  const visibilityHandler = () => {
+    setIsShown(!isShown);
+  };
+
+  // ↓ Redux state management ↓
   const dispatch = useDispatch();
   const counter = useSelector((state) => {
     return state.counter;
@@ -11,18 +20,18 @@ const Counter = () => {
     return state.showCounter;
   });
 
-  const toggleCounterHandler = () => {
-    dispatch({ type: "toggleVisibility" });
-  };
-
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const increaseHandler = (amount) => {
-    dispatch({ type: "increase", amount });
+    dispatch(counterActions.increase(amount));
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
+  };
+
+  const toggleCounterVisibilityHandler = () => {
+    dispatch(counterActions.toggleVisibility());
   };
 
   return (
@@ -34,7 +43,9 @@ const Counter = () => {
         <button onClick={increaseHandler.bind(null, 5)}>Increse by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <button onClick={toggleCounterVisibilityHandler}>
+        Toggle Counter Visibility
+      </button>
     </main>
   );
 };
--- Basic Stuff/redux-dummy-counter/src/store/index.js
+++ Basic Stuff/redux-dummy-counter/src/store/index.js
@@ -1,36 +1,59 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = {
   counter: 0,
   showCounter: true,
 };
 
-const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "increment":
-      return {
-        ...state,
-        counter: state.counter + 1,
-      };
-    case "increase":
-      return {
-        ...state,
-        counter: state.counter + action.amount,
-      };
-    case "decrement":
-      return {
-        ...state,
-        counter: state.counter - 1,
-      };
-    case "toggleVisibility":
-      return {
-        ...state,
-        showCounter: !state.showCounter,
-      };
-  }
-  return state;
-};
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    toggleVisibility(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-const store = createStore(counterReducer);
+const store = configureStore({ reducer: counterSlice.reducer });
+
+export const counterActions = counterSlice.actions;
 
 export default store;
+
+// // This is the 'native' way to create the reducer (without using redux toolkit)
+// const counterReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "increment":
+//       return {
+//         ...state,
+//         counter: state.counter + 1,
+//       };
+//     case "increase":
+//       return {
+//         ...state,
+//         counter: state.counter + action.amount,
+//       };
+//     case "decrement":
+//       return {
+//         ...state,
+//         counter: state.counter - 1,
+//       };
+//     case "toggleVisibility":
+//       return {
+//         ...state,
+//         showCounter: !state.showCounter,
+//       };
+//     default:
+//       return state;
+//   }
+// };
