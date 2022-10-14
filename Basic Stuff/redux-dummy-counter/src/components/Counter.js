610497d82941da865124b21ef249b4ae05a87c9f
Kenji Emura <kenjiemura@gmail.com>
2022-10-14 12:31:56 +0900

    Implemented a simple counter to test State Management using redux and react-redux

--- Basic Stuff/redux-dummy-counter/src/components/Counter.js
+++ Basic Stuff/redux-dummy-counter/src/components/Counter.js
@@ -1,12 +1,39 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => {
+    return state.counter;
+  });
+  const showCounter = useSelector((state) => {
+    return state.showCounter;
+  });
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggleVisibility" });
+  };
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const increaseHandler = (amount) => {
+    dispatch({ type: "increase", amount });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler.bind(null, 5)}>Increse by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
---
+++ Basic Stuff/redux-dummy-counter/src/store/index.js
@@ -0,0 +1,36 @@
+import { createStore } from "redux";
+
+const initialState = {
+  counter: 0,
+  showCounter: true,
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case "increase":
+      return {
+        ...state,
+        counter: state.counter + action.amount,
+      };
+    case "decrement":
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    case "toggleVisibility":
+      return {
+        ...state,
+        showCounter: !state.showCounter,
+      };
+  }
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
