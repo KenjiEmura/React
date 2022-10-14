3aa2ac71d62d63ce9d80fe24a8b11b4abfdd2190
Kenji Emura <kenjiemura@gmail.com>
2022-10-14 23:29:07 +0900

    Refactored the http request login using 'Action Creator Thunk'

--- redux-shopping-cart/src/App.js
+++ redux-shopping-cart/src/App.js
@@ -1,67 +1,28 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+
+import { sendCardData } from "./store/cart-slice";
 
 let isLoadedForTheFirstTime = true;
 
 const App = () => {
   const dispatch = useDispatch();
   const isCartShown = useSelector((state) => state.ui.isCartShown);
-  const cartState = useSelector((state) => state.cart);
+  const cartData = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    console.log("Cart state changed");
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "sending",
-          title: "Sending...",
-          message: "Sending cart data",
-        })
-      );
-      const response = await fetch(
-        "https://react-dummy-project.firebasedatabase.app/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cartState),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data to server failed");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Cart data was sent successfully!",
-        })
-      );
-    };
-
     if (isLoadedForTheFirstTime) {
       isLoadedForTheFirstTime = false;
       return;
     }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: error.message,
-        })
-      );
-      console.log(error.message);
-    });
-  }, [cartState, dispatch]);
+    dispatch(sendCardData(cartData));
+  }, [cartData, dispatch]);
   return (
     <>
       {notification && (
--- redux-shopping-cart/src/store/cart-slice.js
+++ redux-shopping-cart/src/store/cart-slice.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { uiActions } from "./ui-slice";
+
 const initialCartState = {
   items: [],
   totalQuantity: 0,
@@ -39,6 +41,52 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCardData = (cartData) => {
+  console.log("sendCardData triggered");
+  return async (dispatch) => {
+    console.log("return async (dispatch) => {} triggered");
+    dispatch(
+      uiActions.showNotification({
+        status: "sending",
+        title: "Sending...",
+        message: "Sending cart data",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-dummy-project-44c2c-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cartData),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data to server failed");
+      }
+    };
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Cart data was sent successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: error.message,
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
