8f982538b963e96edb468e0df7e1a6e0927508ae
Kenji Emura <kenjiemura@gmail.com>
2022-10-15 13:35:44 +0900

    Implemented programmatic navigation, 404 page and Prompt when user tries to leave the page when data has been already input to the form in QuoteForm

--- routing-spa-quotes/src/pages/QuoteDetail.js
+++ routing-spa-quotes/src/pages/QuoteDetail.js
@@ -1,13 +1,25 @@
 import { Route, useParams } from "react-router-dom";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Kenji", text: "Learning React is fun" },
+  { id: "q2", author: "Hernando", text: "Learning React is great" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((item) => item.id === params.quoteId);
+
+  if (!quote) {
+    return <h1>NOT QUOTE FOUND</h1>;
+  }
+
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
